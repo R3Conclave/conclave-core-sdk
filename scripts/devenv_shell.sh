8db347f82552cd3669d01c1fc79c05b33e97dddd
Mike Hearn <mike@r3.com>
2020-01-27 13:35:47 +0100

    Fix another bug I introduced and make ls use colors.

--- scripts/devenv_shell.sh
+++ scripts/devenv_shell.sh
@@ -12,11 +12,11 @@ echo "You are now in a shell inside a Docker container. "
 echo -e "Run \e[4m./gradlew build test\e[0m to compile and run unit tests."
 echo
 
-docker exec -it -e PS1="conclave \e[32m\$(git branch | awk '/^\* / { print \$2 }')\e[0m \w> "  $@ ${CONTAINER_ID} bash
+docker exec -it -e PS1="conclave \e[32m\$(git branch | awk '/^\* / { print \$2 }')\e[0m \w> " $@ $CONTAINER_ID bash
 
 echo
 echo "The devenv container can be shut down with one of these commands: "
-echo "  - All devenv containers, use: docker stop $(docker ps -f label=sgxjvm -q)"
+echo "  - All devenv containers, use: docker stop \$(docker ps -f label=sgxjvm -q)"
 echo "  - Just this one, use: docker stop $CONTAINER_ID"
 echo
 echo "Ciao!"
