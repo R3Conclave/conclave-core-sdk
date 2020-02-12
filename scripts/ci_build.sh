dbe770b88cfa46338cd37707a229be83288cc619
Rui Almeida <rui.almeida@r3.com>
2020-02-12 09:12:03 +0000

    Disable end-to-end test

--- scripts/ci_build.sh
+++ scripts/ci_build.sh
@@ -21,7 +21,7 @@ runDocker com.r3.sgx/sgxjvm-build "cd $CODE_DOCKER_DIR/samples && \$GRADLE --ref
 runDocker com.r3.sgx/sgxjvm-build "cd $CODE_DOCKER_DIR && \$GRADLE containers:sgxjvm-build:buildImagePublish"
 
 # Then run the tests. We expose the host network so that the test container can connect to the k8s cluster directly.
-runDocker com.r3.sgx/sgxjvm-build "cd $CODE_DOCKER_DIR && \$GRADLE test -i && cd $CODE_DOCKER_DIR/samples && \$GRADLE test -i"
+runDocker com.r3.sgx/sgxjvm-build "cd $CODE_DOCKER_DIR && \$GRADLE test -i && cd $CODE_DOCKER_DIR/samples && \$GRADLE test -i -x end-to-end-test:test"
 
 # Run the sgx-jvm-plugin tests.
 runDocker com.r3.sgx/sgxjvm-build "cd $CODE_DOCKER_DIR/sgx-jvm-plugin && \$GRADLE test -i"
