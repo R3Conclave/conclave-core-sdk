7a3e26420be7d75d8db57337c52118cbef0214db
Mike Hearn <mike@r3.com>
2020-02-19 17:16:50 +0100

    Compatibility fix for macOS which ships a very old bash.

--- scripts/devenv_shell.sh
+++ scripts/devenv_shell.sh
@@ -6,10 +6,10 @@ SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
 source $SCRIPT_DIR/devenv.sh
 
 echo
-echo -e '\e[1m\e[34mWelcome to the Conclave development environment.\e[0m'
+echo -e '\x1B[1m\x1B[34mWelcome to the Conclave development environment.\x1B[0m'
 echo
 echo "You are now in a shell inside a Docker container. "
-echo -e "Run \e[4m./gradlew build test\e[0m to compile and run unit tests."
+echo -e "Run \x1B[4m./gradlew build test\x1B[0m to compile and run unit tests."
 echo
 
 docker exec -it -e PS1="conclave \e[32m\$(git branch | awk '/^\* / { print \$2 }')\e[0m \w> " $@ $CONTAINER_ID bash
