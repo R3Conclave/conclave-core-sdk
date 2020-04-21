0fa1d1018e4b3d3413c1d2c70890582aeb3bc8a4
Mike Hearn <mike@r3.com>
2020-04-21 14:50:37 +0200

    Always print stack traces from Gradle in builds.

--- test-sdk.sh
+++ test-sdk.sh
@@ -4,14 +4,14 @@ set -eou pipefail
 
 echo Building the SDK files without bothering to zip them.
 echo
-./gradlew sdkFiles
+./gradlew --stacktrace sdkFiles
 echo
 echo
 echo Now trying to build and test the hello-world sample
 echo
 (
 cd build/sdk/hello-world;
-./gradlew --no-daemon test host:run
+./gradlew --stacktrace --no-daemon test host:run
 )
 echo Test the sample in debug mode
 (
