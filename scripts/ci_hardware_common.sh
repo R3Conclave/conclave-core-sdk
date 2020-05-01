27e1a356a42724ee177bad08ecb86d924325c189
rui-r3 <49317333+rui-r3@users.noreply.github.com>
2020-05-01 10:02:57 +0100

    [CON-77] Enclave jvm tester pipeline (#334)
    * Add hardware test setup bash functions
    
    * Add execution permission to ci_hardware_tests.sh and ci_enclave_jvm_tester.sh
    
    * Increase enclave-jvm-tester max heap space to 2GB

---
+++ scripts/ci_hardware_common.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -xeuo pipefail
+
+SCRIPT_DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+source ${SCRIPT_DIR}/ci_build_common.sh
+
+function teardownAESM() {
+    docker stop aesmd || true
+    docker rm aesmd || true
+}
+
+function buildAESMImage() {
+    cd $1 && docker build --no-cache -t localhost:5000/com.r3.sgx/aesmd .
+}
+
+function startAESMContainer() {
+    docker run -d --rm --name aesmd ${SGX_HARDWARE_FLAGS} localhost:5000/com.r3.sgx/aesmd
+}
+
+function stopAndRemoveAESMImage() {
+    docker stop aesmd
+    docker rmi localhost:5000/com.r3.sgx/aesmd
+}
