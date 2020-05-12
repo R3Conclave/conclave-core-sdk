f4b8af66e7242a31305a4d03f776a3c8036ee242
Shams Asari <shams.asari@r3.com>
2020-05-12 16:54:34 +0100

    Removed encryption code as it will be replaced by Mail (#352)

--- api-core/api-core-common/src/main/kotlin/com/r3/sgx/core/common/internal/encryption/KeyAgreement.kt
+++
@@ -1,9 +0,0 @@
-package com.r3.sgx.core.common.internal.encryption
-
-/**
- * Stateful representation of shared key-agreement protocol
- */
-interface KeyAgreement {
-    val publicSessionKey: ByteArray
-    fun computeSharedSecret(peerPublicKey: ByteArray): ByteArray
-}
