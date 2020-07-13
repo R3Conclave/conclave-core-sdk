125d71dfd6ef2b324ed94dca2e11a19926824f8c
Roy Hopkins <63246497+roy-hopkins@users.noreply.github.com>
2020-07-13 10:13:58 +0100

    [CON-124]: Trim native-image build to minimum required to build enclaves. (#404)

--- plugin-enclave-gradle/src/main/kotlin/com/r3/conclave/plugin/enclave/gradle/GradleEnclavePlugin.kt
+++ plugin-enclave-gradle/src/main/kotlin/com/r3/conclave/plugin/enclave/gradle/GradleEnclavePlugin.kt
@@ -167,7 +167,7 @@ class GradleEnclavePlugin @Inject constructor(private val layout: ProjectLayout)
             val buildUnsignedGraalEnclaveTask = target.createTask<NativeImage>("buildUnsignedGraalEnclave$type", type, linkerScriptFile) { task ->
                 task.dependsOn(untarGraalVM, copySgxToolsTask, copySubstrateDependenciesTask, generateReflectionConfigTask)
                 task.inputs.files(graalVMDistributionPath, sgxDirectory, substrateDependenciesPath)
-                task.nativeImage.set(target.file("$graalVMDistributionPath/bin/native-image"))
+                task.nativeImage.set(target.file("$graalVMDistributionPath/jre/bin/native-image"))
                 task.jarFile.set(shadowJarTask.archiveFile)
                 task.cLibraryPaths.from("$sgxDirectory/tlibc",
                         "$sgxDirectory/libcxx")
