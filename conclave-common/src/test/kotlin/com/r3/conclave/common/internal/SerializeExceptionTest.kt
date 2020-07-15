92e193d8408469b58d0160ce2a88bf0daa2224fd
Shams Asari <shams.asari@r3.com>
2020-07-15 14:55:46 +0100

    CON-110: Include the cause chain when serialising exceptions (#409)

--- conclave-common/src/test/kotlin/com/r3/conclave/common/internal/SerializeExceptionTest.kt
+++ conclave-common/src/test/kotlin/com/r3/conclave/common/internal/SerializeExceptionTest.kt
@@ -3,26 +3,69 @@ package com.r3.conclave.common.internal
 import com.r3.conclave.utilities.internal.nullableWrite
 import com.r3.conclave.utilities.internal.writeData
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import org.junit.jupiter.params.provider.ValueSource
+import java.util.stream.Stream
 
 class SerializeExceptionTest {
-    @Test
-    fun `round trip with message`() {
-        val original = Exception("BANG")
+    @ParameterizedTest(name = "{displayName} withMessage='{0}' withCause='{1}'")
+    @ArgumentsSource(BooleanPermutations::class)
+    fun `message and cause c'tor`(withMessage: Boolean, withCause: Boolean) {
+        val original = MessageAndCauseException("message here".takeIf { withMessage }, Exception("da cause").takeIf { withCause })
         val roundtrip = SerializeException.deserialise(SerializeException.serialise(original))
         assertExceptionEqual(roundtrip, original)
     }
 
-    @Test
-    fun `round trip with no message`() {
-        val original = Exception()
+    @ParameterizedTest(name = "{displayName} withMessage='{0}' withCause='{1}'")
+    @ArgumentsSource(BooleanPermutations::class)
+    fun `cause and message c'tor`(withMessage: Boolean, withCause: Boolean) {
+        val original = CauseAndMessageException(Exception("da cause").takeIf { withCause }, "message here".takeIf { withMessage })
         val roundtrip = SerializeException.deserialise(SerializeException.serialise(original))
         assertExceptionEqual(roundtrip, original)
     }
 
-    @Test
-    fun `round trip for exception class with no message constructor`() {
-        val original = NoMessageException()
+    @ParameterizedTest(name = "{displayName} withMessage='{0}' withCause='{1}'")
+    @ArgumentsSource(BooleanPermutations::class)
+    fun `message only c'tor`(withMessage: Boolean, withCause: Boolean) {
+        val original = MessageOnlyException("message here".takeIf { withMessage }).initCause(Exception("da cause").takeIf { withCause })
+        val roundtrip = SerializeException.deserialise(SerializeException.serialise(original))
+        assertExceptionEqual(roundtrip, original)
+    }
+
+    @ParameterizedTest(name = "{displayName} withCause='{0}'")
+    @ValueSource(booleans = [true, false])
+    fun `cause only c'tor`(withCause: Boolean) {
+        val original = CauseOnlyException(Exception("da cause").takeIf { withCause })
+        val roundtrip = SerializeException.deserialise(SerializeException.serialise(original))
+        assertExceptionEqual(roundtrip, original)
+    }
+
+    @ParameterizedTest(name = "{displayName} withCause='{0}'")
+    @ValueSource(booleans = [true, false])
+    fun `cause and constructed message c'tor`(withCause: Boolean) {
+        val original = CauseWithConstructedMessageException(Exception("da cause").takeIf { withCause })
+        val roundtrip = SerializeException.deserialise(SerializeException.serialise(original))
+        assertExceptionEqual(roundtrip, original)
+    }
+
+    @ParameterizedTest(name = "{displayName} withCause='{0}'")
+    @ValueSource(booleans = [true, false])
+    fun `empty c'tor`(withCause: Boolean) {
+        val original = EmptyException().initCause(Exception("da cause").takeIf { withCause })
+        val roundtrip = SerializeException.deserialise(SerializeException.serialise(original))
+        assertExceptionEqual(roundtrip, original)
+    }
+
+    @ParameterizedTest(name = "{displayName} withCause='{0}'")
+    @ValueSource(booleans = [true, false])
+    fun `empty with constructed message c'tor`(withCause: Boolean) {
+        val original = EmptyWithConstructedMessageException().initCause(Exception("da cause").takeIf { withCause })
         val roundtrip = SerializeException.deserialise(SerializeException.serialise(original))
         assertExceptionEqual(roundtrip, original)
     }
@@ -30,14 +73,16 @@ class SerializeExceptionTest {
     @Test
     fun `deserialising unknown exception`() {
         val serialised = writeData {
+            writeInt(1) // Cause chain count
             writeUTF("com.foo.bar.Exception")
             nullableWrite("BOOM") { writeUTF(it) }
-            writeInt(0)
+            writeInt(0) // Stacktrace
         }
 
         val deserialised = SerializeException.deserialise(serialised)
         assertThat(deserialised).isExactlyInstanceOf(RuntimeException::class.java)
         assertThat(deserialised).hasMessage("com.foo.bar.Exception: BOOM")
+        assertThat(deserialised).hasNoCause()
     }
 
     private fun assertExceptionEqual(actual: Throwable, expected: Throwable) {
@@ -55,7 +100,30 @@ class SerializeExceptionTest {
             assertThat(actualStackTrace[it].fileName).isEqualTo(expectedStackTrace[it].fileName)
             assertThat(actualStackTrace[it].lineNumber).isEqualTo(expectedStackTrace[it].lineNumber)
         }
+        if (expected.cause != null) {
+            assertNotNull(actual.cause) { "Expected $actual to have a cause but it doesn't" }
+            assertExceptionEqual(actual.cause!!, expected.cause!!)
+        } else {
+            assertThat(actual).hasNoCause()
+        }
     }
 
-    class NoMessageException : Exception()
+    class MessageAndCauseException(message: String?, cause: Throwable?) : Exception(message, cause)
+    class CauseAndMessageException(cause: Throwable?, message: String?) : Exception(message, cause)
+    class MessageOnlyException(message: String?) : Exception(message)
+    class CauseOnlyException(cause: Throwable?) : Exception(cause)
+    class CauseWithConstructedMessageException(cause: Throwable?) : Exception("Constructed message", cause)
+    class EmptyException : Exception()
+    class EmptyWithConstructedMessageException : Exception("Constructed message")
+
+    class BooleanPermutations : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> {
+            return Stream.of(
+                    Arguments.of(true, true),
+                    Arguments.of(true, false),
+                    Arguments.of(false, true),
+                    Arguments.of(false, false)
+            )
+        }
+    }
 }
