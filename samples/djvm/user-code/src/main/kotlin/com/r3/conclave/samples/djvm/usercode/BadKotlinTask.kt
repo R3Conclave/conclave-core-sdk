3fea77631488531eddfb968734ea583656668aed
Shams Asari <shams.asari@r3.com>
2020-09-16 19:15:12 +0100

    Removed the DJVM enclave sample (#480)
    DJVM integration is more comprehensively covered by enclave-jvm-tester.

--- samples/djvm/user-code/src/main/kotlin/com/r3/conclave/samples/djvm/usercode/BadKotlinTask.kt
+++
@@ -1,9 +0,0 @@
-package com.r3.conclave.samples.djvm.usercode
-
-import java.util.function.Function
-
-class BadKotlinTask : Function<String, Long> {
-    override fun apply(input: String): Long {
-        return javaClass.getField(input).getLong(this)
-    }
-}
