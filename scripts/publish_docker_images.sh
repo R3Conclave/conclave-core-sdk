2740d151f21d2438b403f90ce17f2ec37f143406
Rui Almeida <49317333+rui-r3@users.noreply.github.com>
2021-02-05 15:55:36 +0000

    Update docker images version, maintainer and remove duplicate code
    The `LABEL version` seems to be just an image property and is unrelated with the tag. I think it would be nice to match it with the Conclave version to avoid misunderstandings.
    
    `publish_docker_images.sh` contained duplicate code since part of it has been included in `ci_build_common.sh`. This would cause the publish docker images TC pipeline to build the images twice.

--- scripts/publish_docker_images.sh
+++ scripts/publish_docker_images.sh
@@ -4,15 +4,5 @@ set -xeuo pipefail
 SCRIPT_DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 source ${SCRIPT_DIR}/ci_build_common.sh
 
-# Prune docker images
-docker image prune -af
-
-# Login and pull the current build image
-docker login ${OBLIVIUM_CONTAINER_REGISTRY_URL} -u ${OBLIVIUM_CONTAINER_REGISTRY_USERNAME} -p ${OBLIVIUM_CONTAINER_REGISTRY_PASSWORD}
-docker pull ${OBLIVIUM_CONTAINER_REGISTRY_URL}/com.r3.sgx/sgxjvm-build
-
-# Refresh dependencies
-runDocker com.r3.sgx/sgxjvm-build "cd $CODE_DOCKER_DIR && \$GRADLE --refresh-dependencies -i"
-
 # Publish docker images with latest tag
 runDocker com.r3.sgx/sgxjvm-build "cd $CODE_DOCKER_DIR && \$GRADLE -i containers:publishAsLatest"
