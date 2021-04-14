615a81af99aa18749736cb2d17668fc466ac8219
Rui Almeida <49317333+rui-alm@users.noreply.github.com>
2021-04-14 10:25:54 +0100

    [CON-316] Fix build image load command on publish script. Fix argument checking for release

--- scripts/ci_publish_docker_images.sh
+++ scripts/ci_publish_docker_images.sh
@@ -2,15 +2,16 @@
 set -xeuo pipefail
 
 script_dir=$(dirname ${BASH_SOURCE[0]})
+source ${script_dir}/ci_build_common.sh
 source ${script_dir}/ci_hardware_common.sh
 
 loadAESMImage
-docker load -i $script_dir/../containers/sgxjvm-build/src/docker/sgxjvm-build-docker-image.tar
+loadBuildImage
 
 release_mode="${1:-}"
 conclave_version=`grep "conclave_version" ${script_dir}/../versions.gradle | cut -d"'" -f2`
 
-if [ $release_mode == "release" ]; then
+if [ "$release_mode" == "release" ]; then
     docker tag $OBLIVIUM_CONTAINER_REGISTRY_URL/com.r3.sgx/aesmd:latest $OBLIVIUM_CONTAINER_RELEASE_REGISTRY_URL/com.r3.sgx/aesmd:$conclave_version
     docker tag $OBLIVIUM_CONTAINER_REGISTRY_URL/com.r3.sgx/sgxjvm-build:latest $OBLIVIUM_CONTAINER_RELEASE_REGISTRY_URL/com.r3.sgx/sgxjvm-build:$conclave_version
     docker push $OBLIVIUM_CONTAINER_RELEASE_REGISTRY_URL/com.r3.sgx/aesmd:$conclave_version
