65322be17aef88e2c39dc86e975f66e3c8a258fe
Denis Zhereschin <62896873+deniszhereschinr3@users.noreply.github.com>
2021-06-02 10:40:20 +0100

    CON-387: Support mock mode in CorDapp sample tests (#697)

--- docs/sdk-extras/cordapp/workflows/src/test/java/com/r3/conclave/cordapp/sample/host/ReverseStringEnclaveTest.java
+++ docs/sdk-extras/cordapp/workflows/src/test/java/com/r3/conclave/cordapp/sample/host/ReverseStringEnclaveTest.java
@@ -24,6 +24,7 @@ class ReverseStringEnclaveTest {
     //
     // Obviously in a real app you'd not use SEC:INSECURE, however this makes the sample work in simulation mode.
     private final String constraint = "S:4924CA3A9C8241A3C0AA1A24A407AA86401D2B79FA9FF84932DA798A942166D4 PROD:1 SEC:INSECURE";
+    private final String mock_constraint = "S:0000000000000000000000000000000000000000000000000000000000000000 PROD:1 SEC:INSECURE";
 
     @BeforeAll
     static void setup() {
@@ -47,8 +48,21 @@ class ReverseStringEnclaveTest {
 
     @Test
     public void reverseString() throws ExecutionException, InterruptedException {
-        CordaFuture<String> flow = client.startFlow(new ReverseFlow(host.getInfo().getLegalIdentities().get(0), "zipzop", constraint));
+        CordaFuture<String> flow = client.startFlow(
+                new ReverseFlow(
+                        host.getInfo().getLegalIdentities().get(0),
+                        "zipzop",
+                        getConstraint()
+                )
+        );
         network.runNetwork();
         assertEquals("pozpiz", flow.get());
+    }
+
+    private String getConstraint() {
+        String mode = System.getProperty("enclaveMode");
+        if (mode == null || !mode.toLowerCase().equals("mock"))
+            return constraint;
+        return mock_constraint;
     }
 }
