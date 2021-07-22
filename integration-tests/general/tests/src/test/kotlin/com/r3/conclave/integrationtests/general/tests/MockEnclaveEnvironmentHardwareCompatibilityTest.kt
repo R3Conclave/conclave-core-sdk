92c2e81d286037c6594835e14c1510be8d6e1065
Marco Bonifazi <marco.bonifazi@r3.com>
2021-07-22 19:04:34 +0000

    Remove commented code

--- integration-tests/general/tests/src/test/kotlin/com/r3/conclave/integrationtests/general/tests/MockEnclaveEnvironmentHardwareCompatibilityTest.kt
+++ integration-tests/general/tests/src/test/kotlin/com/r3/conclave/integrationtests/general/tests/MockEnclaveEnvironmentHardwareCompatibilityTest.kt
@@ -130,7 +130,6 @@ class MockEnclaveEnvironmentHardwareCompatibilityTest : HardwareTest {
             }
 
             host.start(attestationParameters, null)
-            //check(host.enclaveMode == EnclaveMode.DEBUG)
             host
         }
     }
