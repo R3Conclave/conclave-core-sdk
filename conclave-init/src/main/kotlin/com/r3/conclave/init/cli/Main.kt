467ad2bbb1080e588b6894deedd214788527ea78
Rusheb Shah <rusheb.shah@r3.com>
2021-11-01 12:46:02 +0000

    CON-739 Get SDK Version from SDK repo rather than conclave-init.jar (#881)
    This allows conclave-init to be used with any version of the SDK, including previous versions.
    
    It also uses JarInputStream and java.util.jar.Manifest to simplify the code.
    
    Note: Main contains an increasing amount of logic that is not tested. May need to do something about this soon.

--- conclave-init/src/main/kotlin/com/r3/conclave/init/cli/Main.kt
+++ conclave-init/src/main/kotlin/com/r3/conclave/init/cli/Main.kt
@@ -1,11 +1,14 @@
 package com.r3.conclave.init.cli
 
 import com.r3.conclave.init.ConclaveInit
-import com.r3.conclave.init.template.ManifestFiles
+import com.r3.conclave.init.common.walkTopDown
 import picocli.CommandLine
-import java.io.IOException
+import java.nio.file.Path
+import java.util.jar.JarInputStream
 import kotlin.io.path.absolutePathString
 import kotlin.io.path.exists
+import kotlin.io.path.extension
+import kotlin.io.path.inputStream
 import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
@@ -26,8 +29,10 @@ fun main(args: Array<String>) {
     with(params) {
         checkTargetDoesNotExist(this)
 
+        val conclaveVersion = getConclaveVersion(sdkRepo)
+
         println(projectSummary())
-        ConclaveInit.createProject(language, basePackage, enclaveClass, target, sdkRepo, getConclaveVersion())
+        ConclaveInit.createProject(language, basePackage, enclaveClass, target, sdkRepo, conclaveVersion)
     }
 }
 
@@ -53,14 +58,22 @@ private fun checkTargetDoesNotExist(params: CommandLineParameters) {
     }
 }
 
-private fun getConclaveVersion(): String = try {
-    ManifestFiles.load().conclaveVersion
-} catch (e: IOException) {
-    System.err.println(
-        "Error: could not detect ConclaveVersion. " +
-                "Please add it manually to gradle.properties of the generated project"
-    )
-    returnEmptyString()
-}
 
-private fun returnEmptyString(): String = ""
+private fun getConclaveVersion(sdkRepo: Path): String {
+    val jarsFromRepo = sdkRepo.walkTopDown().filter { it.extension == "jar" }
+    val conclaveVersion = jarsFromRepo.firstNotNullOfOrNull { jar ->
+        val manifest = jar.inputStream().use { JarInputStream(it).manifest }
+        manifest.mainAttributes.getValue("Conclave-Release-Version")
+    }
+
+    if (conclaveVersion == null) {
+        System.err.println(
+            "Error: could not detect Conclave version of repo $sdkRepo. Please add it manually " +
+                    "to the gradle.properties file in the root of the generated project."
+        )
+
+        return ""
+    }
+
+    return conclaveVersion
+}
