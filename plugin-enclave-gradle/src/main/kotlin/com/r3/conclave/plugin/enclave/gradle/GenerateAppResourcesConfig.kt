f10032cd32ffcb96a8879a399a8f34ccd53c47dc
Marco Bonifazi <85171431+bonifazi-r3@users.noreply.github.com>
2021-11-11 11:45:14 +0000

    CON-770: Replace backward with forward strings in app-resources-config.json on Windows
    # Changes
    When building the `conclave-kms` repo, we get this error
    ```
    S C:\Users\RamjiKerai\workspace\conclave-kms> ./gradlew :kms-host:assemble -PenclaveMode=simulation
    Starting a Gradle Daemon, 1 incompatible and 3 stopped Daemons could not be reused, use --status for details
    
    > Task :kms-enclave:buildUnsignedGraalEnclaveSimulation FAILED
    Warning: Ignoring server-mode native-image argument --no-server.
    Fatal error:java.util.regex.PatternSyntaxException: \k is not followed by '<' for named capturing group near index 8
    com\r3\keyservice\enclave\enclave.properties
            ^
            at java.base/java.util.regex.Pattern.error(Pattern.java:2027)
            at java.base/java.util.regex.Pattern.escape(Pattern.java:2545)
            at java.base/java.util.regex.Pattern.atom(Pattern.java:2285)
            at java.base/java.util.regex.Pattern.sequence(Pattern.java:2209)
            at java.base/java.util.regex.Pattern.expr(Pattern.java:2068)
            at java.base/java.util.regex.Pattern.compile(Pattern.java:1782)
            at java.base/java.util.regex.Pattern.<init>(Pattern.java:1428)
    ```
    This is due to an issue with Windows paths vs Linux paths.
    
    Specifically when we generate the file `app-resource-config.json` we add paths into it. These paths contain backward slashes as the related Gradle that generates the file is run on Windows.
    
    When we run `native-image` we do it inside a Docker Linux container and that file is read as if we were on a Linux OS, hence causing the pattern matching failure. The fix consists in replacing the backward slashes with forward slashes and this is what we do here.
    
    To investigate, we needed to go into the docker container and see the file that got generated, @ac101m helped on this
    
    
    # Reviewer Checklist
    
    - [ ] Are you happy with the code changes?
    - [ ] Is there adequate unit test coverage?
    - [ ] Has the external docsite been updated?
    - [ ] Have the API docs (KDocs) been updated?
    - [ ] Has the internal docsite been updated?
    - [ ] Has an item been added to the [Release notes](https://github.com/corda/sgxjvm/blob/master/docs/docs/index.md)?
    - [ ] Have all dependencies introduced by this PR been approved?
    - [ ] Has licences/3rd-party.md been updated to reflect any new dependencies?
    - [ ] If the gradle plugin has been updated, have the gradle plugin tests been run in TeamCity?

--- plugin-enclave-gradle/src/main/kotlin/com/r3/conclave/plugin/enclave/gradle/GenerateAppResourcesConfig.kt
+++ plugin-enclave-gradle/src/main/kotlin/com/r3/conclave/plugin/enclave/gradle/GenerateAppResourcesConfig.kt
@@ -5,6 +5,7 @@ import org.gradle.api.model.ObjectFactory
 import org.gradle.api.provider.Property
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.OutputFile
+import org.gradle.internal.os.OperatingSystem
 import java.io.File
 import javax.inject.Inject
 
@@ -18,8 +19,16 @@ open class GenerateAppResourcesConfig @Inject constructor(objects: ObjectFactory
     override fun action() {
         val resourcesDirectory = resourcesDirectory.get()
         val resourceFiles = resourcesDirectory.walkTopDown().filter { it.isFile }
-        val resourceFilesRelative = resourceFiles.map { it.relativeTo(resourcesDirectory).toString() }.toList()
 
+        val resourceFilesRelative = if (OperatingSystem.current().isWindows) {
+            //  This task saves path strings in the appResourcesConfigFile below, and as we are running it on Windows,
+            //    the paths will contain backward slashes and won't be read correctly when running native-image in the Docker container.
+            //    Therefore we need to replace the backward slashes with forward slashes.
+            resourceFiles.map { it.relativeTo(resourcesDirectory).toString().replace("\\", "/") }.toList()
+        } else {
+            resourceFiles.map { it.relativeTo(resourcesDirectory).toString() }.toList()
+        }
         ResourcesConfig.create(includes = resourceFilesRelative).writeToFile(appResourcesConfigFile.get().asFile)
+
     }
 }
