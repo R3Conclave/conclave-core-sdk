ed1ce3788a7ad1a5759a21d2229bb0905e88450a
Rui Almeida <49317333+rui-alm@users.noreply.github.com>
2022-06-20 14:20:54 +0100

    [CCL-435] Add separate scans for SDK components. Add licence files placeholders.
    I've updated the TeamCity artifact dependencies so the Nexus IQ scan can find the new artifacts. You may need to update your branches.
    
    Nexus IQ isn't doing a great job at detecting the dependencies so some of the reports are incomplete.
    
    The license text should be provided by legal.
    
    @chagonr3

--- scripts/nexus-iq_publish.sh
+++ scripts/nexus-iq_publish.sh
@@ -25,6 +25,14 @@ java --add-opens java.base/java.util=ALL-UNNAMED --add-opens java.base/java.lang
     -r "${script_dir}/../build/nexus-iq/nexus-iq_report.json" \
     "${script_dir}/../build/distributions/conclave-sdk-${version}.zip"
 
+java --add-opens java.base/java.util=ALL-UNNAMED --add-opens java.base/java.lang=ALL-UNNAMED -jar $NEXUS_IQ_HOME/nexus-iq-cli.jar \
+    -s https://nexusiq.dev.r3.com \
+    -i conclave_sdk_conclave_init \
+    -a $1:$2 \
+    -t $3 \
+    -r "${script_dir}/../conclave-init/build/nexus-iq/nexus-iq_report.json" \
+    "${script_dir}/../conclave-init/build/libs/conclave-init-shadow.jar"
+
 res=$?
 
 if [ $res -eq 0 ]
