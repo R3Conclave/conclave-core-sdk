37fa55e78ef8bf7cba23327c9175c114ac69a765
Alexander <alex.cramb@r3.com>
2022-09-21 16:36:42 +0100

    Add buffer copying to MockCallInterfaceConnector

--- conclave-common/src/main/kotlin/com/r3/conclave/common/MockCallInterfaceConnector.kt
+++ conclave-common/src/main/kotlin/com/r3/conclave/common/MockCallInterfaceConnector.kt
@@ -5,14 +5,24 @@ import com.r3.conclave.common.internal.EnclaveCallType
 import com.r3.conclave.common.internal.HostCallType
 import com.r3.conclave.common.internal.ThreadLocalEnclaveContext
 import com.r3.conclave.utilities.internal.EnclaveContext
+import com.r3.conclave.utilities.internal.getRemainingBytes
 import java.nio.ByteBuffer
 
 /**
  * This class serves as the glue between the host and a mock enclave.
  * An object of this class exists "between" the mock enclave and the enclave host and servers
  * to connect the MockHostCallInterface to the MockEnclaveCallInterface.
+ * Parameter and return value buffers are deep copied to better emulate the behaviour of a real enclave.
  */
 class MockCallInterfaceConnector {
+    companion object {
+        private fun copyBuffer(buffer: ByteBuffer): ByteBuffer {
+            val tmp = buffer.duplicate()
+            tmp.rewind()
+            return ByteBuffer.wrap(tmp.getRemainingBytes())
+        }
+    }
+
     private lateinit var enclaveCallAcceptor: CallAcceptor<EnclaveCallType>
     private lateinit var hostCallAcceptor: CallAcceptor<HostCallType>
 
@@ -28,7 +38,9 @@ class MockCallInterfaceConnector {
         check(EnclaveContext.isInsideEnclave())
         ThreadLocalEnclaveContext.set(false)
         try {
-            return hostCallAcceptor.acceptCall(callType, parameterBuffer)
+            val parameterBufferCopy = copyBuffer(parameterBuffer)
+            val returnBuffer = hostCallAcceptor.acceptCall(callType, parameterBufferCopy)
+            return copyBuffer(returnBuffer)
         } finally {
             ThreadLocalEnclaveContext.set(true)
         }
@@ -38,7 +50,9 @@ class MockCallInterfaceConnector {
         check(!EnclaveContext.isInsideEnclave())
         ThreadLocalEnclaveContext.set(true)
         try {
-            return enclaveCallAcceptor.acceptCall(callType, parameterBuffer)
+            val parameterBufferCopy = copyBuffer(parameterBuffer)
+            val returnBuffer = enclaveCallAcceptor.acceptCall(callType, parameterBufferCopy)
+            return copyBuffer(returnBuffer)
         } finally {
             ThreadLocalEnclaveContext.set(false)
         }
