33b0686518527c23eb1c6e43811702a1306507c9
Alexander Cramb <alex.cramb@r3.com>
2022-10-07 11:56:38 +0100

    Move bracket to new line

--- conclave-common/src/main/kotlin/com/r3/conclave/common/internal/CallInterfaceStackFrame.kt
+++ conclave-common/src/main/kotlin/com/r3/conclave/common/internal/CallInterfaceStackFrame.kt
@@ -5,4 +5,5 @@ import java.nio.ByteBuffer
 class CallInterfaceStackFrame<CALL_TYPE>(
         val callType: CALL_TYPE,
         var returnBuffer: ByteBuffer? = null,
-        var exceptionBuffer: ByteBuffer? = null)
+        var exceptionBuffer: ByteBuffer? = null
+)
