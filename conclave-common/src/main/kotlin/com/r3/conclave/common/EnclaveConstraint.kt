bf25a218cef428ccafe8fafbe229e3cb64c498aa
Filipe Oliveira <filipe.oliveira@r3.com>
2022-10-10 10:23:44 +0100

    CON-806 Updated the docs

--- conclave-common/src/main/kotlin/com/r3/conclave/common/EnclaveConstraint.kt
+++ conclave-common/src/main/kotlin/com/r3/conclave/common/EnclaveConstraint.kt
@@ -69,8 +69,10 @@ class EnclaveConstraint {
 
     /**
      * Whether to accept debug/insecure enclaves, or enclaves running on hosts that have fallen
-     * behind on their security patches. By default stale machines (running old software/microcode) are accepted, to avoid
+     * behind on their security patches. By default, stale machines (running old software/microcode) are accepted, to avoid
      * outages in case of operator laxness, but you can tighten this if wanted.
+     * N.B. that only insecure enclaves are accepted if the security level is set to insecure. This prevents clients
+     * from connecting to enclaves running in production by mistake.
      */
     var minSecurityLevel: EnclaveSecurityInfo.Summary = EnclaveSecurityInfo.Summary.STALE
 
