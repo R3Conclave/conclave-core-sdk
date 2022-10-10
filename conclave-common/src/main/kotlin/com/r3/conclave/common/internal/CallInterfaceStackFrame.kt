082a17a1dc4f9eb18f89a55931df1328edd0a274
Alexander Cramb <alex.cramb@r3.com>
2022-10-10 12:55:53 +0100

    Add Kdoc to CallInterfaceStackFrame

--- conclave-common/src/main/kotlin/com/r3/conclave/common/internal/CallInterfaceStackFrame.kt
+++ conclave-common/src/main/kotlin/com/r3/conclave/common/internal/CallInterfaceStackFrame.kt
@@ -2,6 +2,11 @@ package com.r3.conclave.common.internal
 
 import java.nio.ByteBuffer
 
+/**
+ * This is used in the call interface to relate return or exception messages back to the call message which
+ * they are intended for. See [com.r3.conclave.enclave.internal.NativeEnclaveHostInterface] and
+ * [com.r3.conclave.host.internal.NativeHostEnclaveInterface].
+ */
 class CallInterfaceStackFrame<CALL_TYPE>(
         val callType: CALL_TYPE,
         var returnBuffer: ByteBuffer? = null,
