4f807a763b15942c5498ad36c67e4d2569983b31
Filipe Oliveira <filipe.oliveira@r3.com>
2022-10-13 21:07:05 +0100

    Test

--- scripts/ci_build.sh
+++ scripts/ci_build.sh
@@ -4,4 +4,4 @@ set -xeuo pipefail
 script_dir=$(dirname ${BASH_SOURCE[0]})
 source ${script_dir}/ci_build_common.sh
 
-runDocker $container_image_sdk_build "./gradlew test publishAllPublicationsToBuildRepository -s -i"
+runDocker $container_image_sdk_build "./gradlew buildDocs test publishAllPublicationsToBuildRepository -s -i"
