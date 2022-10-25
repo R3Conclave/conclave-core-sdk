3843401d13e4e7d93688a575b480fd0f9b9851c7
Marco Bonifazi <marco.bonifazi@r3.com>
2022-10-25 19:43:04 +0100

    Remove import

--- integration-tests/general/common-test/src/main/kotlin/com/r3/conclave/integrationtests/general/commontest/TestUtils.kt
+++ integration-tests/general/common-test/src/main/kotlin/com/r3/conclave/integrationtests/general/commontest/TestUtils.kt
@@ -1,6 +1,5 @@
 package com.r3.conclave.integrationtests.general.commontest
 
-import com.r3.conclave.common.OpaqueBytes
 import com.r3.conclave.host.AttestationParameters
 import com.r3.conclave.host.EnclaveHost
 import java.nio.file.Path
