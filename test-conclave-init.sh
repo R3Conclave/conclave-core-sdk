15833cf79c4b13b0f61a2db96f2f11968228d9e8
Anna Paskova <anna.paskova@r3.com>
2022-11-08 15:32:40 +0000

    CON-1137: Show version number in Conclave Init with --version option

--- test-conclave-init.sh
+++ test-conclave-init.sh
@@ -5,13 +5,22 @@ set -eoux pipefail
 # This script requires a local build of the SDK at build/repo, which can be produced by running
 # ./gradlew publishAllPublicationsToBuildRepository.
 
+CONCLAVE_SDK_VERSION=$(./gradlew -q properties | grep -w "conclave_version" | awk '{print $2}')
+
 echo
 echo Testing Conclave Init
 echo
 
 pushd build
 
-conclaveInitJar=$(find repo/com/r3/conclave/conclave-init/ -name 'conclave-init-*jar' -not -name 'conclave-init-*javadoc.jar' -not -name 'conclave-init-*-sources.jar')
+# Find the latest created Conclave Init .jar file. %T@ prints file's last modification time in seconds since Jan. 1, 1970, 00:00 GMT which is used for sorting.
+conclaveInitJar=$(find repo/com/r3/conclave/conclave-init/ -name 'conclave-init-*jar' -not -name 'conclave-init-*javadoc.jar' -not -name 'conclave-init-*-sources.jar' -printf "%T@ %p\n" | sort -n | tail -n 1 | awk '{print $2}')
+
+conclaveInitVersion=$("$JAVA_HOME"/bin/java -jar "$conclaveInitJar" -V | awk '{print $3}')
+if [ "$conclaveInitVersion" != "$CONCLAVE_SDK_VERSION" ]; then
+  echo "Conclave SDK version does not match the one in the Conclave Init."
+  exit 1
+fi
 
 echo Create Java project
 
