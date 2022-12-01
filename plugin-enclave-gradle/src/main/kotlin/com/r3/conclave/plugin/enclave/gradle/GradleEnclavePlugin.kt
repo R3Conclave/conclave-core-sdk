f662d364d0fa38f77fbbca173dd8e8ba644ca69c
Alexander <alex.cramb@r3.com>
2022-12-01 11:44:20 +0000

    Fix indentation of generateEnclaveMetadata task

--- plugin-enclave-gradle/src/main/kotlin/com/r3/conclave/plugin/enclave/gradle/GradleEnclavePlugin.kt
+++ plugin-enclave-gradle/src/main/kotlin/com/r3/conclave/plugin/enclave/gradle/GradleEnclavePlugin.kt
@@ -373,34 +373,34 @@ class GradleEnclavePlugin @Inject constructor(private val layout: ProjectLayout)
                 type,
                 linuxExec
             ) { task ->
-                    val signingTask = enclaveExtension.signingType.map {
-                        when (it) {
-                            SigningType.DummyKey -> signEnclaveWithKeyTask
-                            SigningType.PrivateKey -> signEnclaveWithKeyTask
-                            else -> addEnclaveSignatureTask
-                        }
+                val signingTask = enclaveExtension.signingType.map {
+                    when (it) {
+                        SigningType.DummyKey -> signEnclaveWithKeyTask
+                        SigningType.PrivateKey -> signEnclaveWithKeyTask
+                        else -> addEnclaveSignatureTask
                     }
-                    task.dependsOn(signingTask)
-                    val signedEnclaveFile = enclaveExtension.signingType.flatMap {
-                        when (it) {
-                            SigningType.DummyKey -> signEnclaveWithKeyTask.outputSignedEnclave
-                            SigningType.PrivateKey -> signEnclaveWithKeyTask.outputSignedEnclave
-                            else -> {
-                                if (!enclaveExtension.mrsignerPublicKey.isPresent) {
-                                    throwMissingConfigForExternalSigning("mrsignerPublicKey")
-                                }
-                                if (!enclaveExtension.mrsignerSignature.isPresent) {
-                                    throwMissingConfigForExternalSigning("mrsignerSignature")
-                                }
-                                addEnclaveSignatureTask.outputSignedEnclave
-                            }
-                        }
-                    }
-                    task.inputSignedEnclave.set(signedEnclaveFile)
-                    task.inputs.files(signedEnclaveFile)
-                    task.mrsignerOutputFile.set(enclaveDirectory.resolve(ENCLAVE_MRSIGNER_FILE).toFile())
-                    task.mrenclaveOutputFile.set(enclaveDirectory.resolve(ENCLAVE_MRENCLAVE_FILE).toFile())
                 }
+                task.dependsOn(signingTask)
+                val signedEnclaveFile = enclaveExtension.signingType.flatMap {
+                    when (it) {
+                        SigningType.DummyKey -> signEnclaveWithKeyTask.outputSignedEnclave
+                        SigningType.PrivateKey -> signEnclaveWithKeyTask.outputSignedEnclave
+                        else -> {
+                            if (!enclaveExtension.mrsignerPublicKey.isPresent) {
+                                throwMissingConfigForExternalSigning("mrsignerPublicKey")
+                            }
+                            if (!enclaveExtension.mrsignerSignature.isPresent) {
+                                throwMissingConfigForExternalSigning("mrsignerSignature")
+                            }
+                            addEnclaveSignatureTask.outputSignedEnclave
+                        }
+                    }
+                }
+                task.inputSignedEnclave.set(signedEnclaveFile)
+                task.inputs.files(signedEnclaveFile)
+                task.mrsignerOutputFile.set(enclaveDirectory.resolve(ENCLAVE_MRSIGNER_FILE).toFile())
+                task.mrenclaveOutputFile.set(enclaveDirectory.resolve(ENCLAVE_MRENCLAVE_FILE).toFile())
+            }
 
             val buildSignedEnclaveTask = target.createTask<BuildSignedEnclave>("buildSignedEnclave$type") { task ->
                 task.dependsOn(generateEnclaveMetadataTask)
