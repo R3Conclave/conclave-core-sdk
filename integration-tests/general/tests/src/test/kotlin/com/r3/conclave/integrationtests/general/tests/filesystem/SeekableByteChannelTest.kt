8e7d07284242e1cada2dbac6799896a881374829
Anna Paskova <anna.paskova@r3.com>
2022-12-20 15:33:21 +0000

    Another set of Gramine tests are disabled as they are failing

--- integration-tests/general/tests/src/test/kotlin/com/r3/conclave/integrationtests/general/tests/filesystem/SeekableByteChannelTest.kt
+++ integration-tests/general/tests/src/test/kotlin/com/r3/conclave/integrationtests/general/tests/filesystem/SeekableByteChannelTest.kt
@@ -2,6 +2,7 @@ package com.r3.conclave.integrationtests.general.tests.filesystem
 
 import com.r3.conclave.integrationtests.general.common.tasks.CloseByteChannel
 import com.r3.conclave.integrationtests.general.common.tasks.NewDeleteOnCloseByteChannel
+import com.r3.conclave.integrationtests.general.commontest.TestUtils.graalvmOnlyTest
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
@@ -25,6 +26,7 @@ class SeekableByteChannelTest : FileSystemEnclaveTest() {
         "/tmp/bytechannel-delete-on-close.data, false"
     )
     fun byteChannelDeleteOnClose(path: String, nioApi: Boolean) {
+        graalvmOnlyTest() // CON-1264: Gramine: accessing filesystem and devices causes InvalidKeyException: Invalid AES key length: 0 bytes
         Handler(uid.getAndIncrement(), path).close()
         fileInputStreamNonExistingFile(path, nioApi)
     }
