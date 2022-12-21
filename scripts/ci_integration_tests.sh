17fcac437a412ad93682c6041a063e2800ac3f86
Anna Paskova <anna.paskova@r3.com>
2022-12-21 15:34:17 +0000

    Convert parameters to lowercase and fix checks

--- scripts/ci_integration_tests.sh
+++ scripts/ci_integration_tests.sh
@@ -4,11 +4,11 @@ set -xeuo pipefail
 script_dir=$(dirname ${BASH_SOURCE[0]})
 source ${script_dir}/ci_build_common.sh
 
-# Set first char to uppercase
-enclaveMode=${1^}
-runtimeType=${2^}
+# Set parameters to lowercase
+enclaveMode=${1,,}
+runtimeType=${2,,}
 
-if [ "$enclaveMode" != "Simulation" ] && [ "$runtimeType" == "Gramine" ]; then
+if [ "$enclaveMode" != "simulation" ] && [ "$runtimeType" == "gramine" ]; then
     # Hardware tests for Gramine need the AESM service running.
     # Here we teardown any AESMD container that might be left running, build and start the AESMD container.
     docker stop aesmd || true
@@ -18,7 +18,7 @@ fi
 # Gramine needs a custom seccomp profile to work. https://github.com/gramineproject/gramine/issues/164#issuecomment-949349475
 runDocker "$container_image_integration_tests_build" "cd integration-tests && ./gradlew -PenclaveMode=$enclaveMode -PruntimeType=$runtimeType test -s -i" "--security-opt seccomp=$script_dir/gramine-seccomp.json"
 
-if [ "$enclaveMode" != "Simulation" ] && [ "$runtimeType" == "Gramine" ]; then
+if [ "$enclaveMode" != "simulation" ] && [ "$runtimeType" == "gramine" ]; then
     # Teardown AESM container
     docker stop aesmd || true
 fi
