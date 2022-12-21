a60499f64fb7e43492dd02e05aa1d55335efd8f1
Anna Paskova <anna.paskova@r3.com>
2022-12-21 14:28:17 +0000

    Disable failing gramine debug test with multi threads

--- integration-tests/general/tests/src/test/kotlin/com/r3/conclave/integrationtests/general/tests/ThreadSafeEnclaveTests.kt
+++ integration-tests/general/tests/src/test/kotlin/com/r3/conclave/integrationtests/general/tests/ThreadSafeEnclaveTests.kt
@@ -1,13 +1,17 @@
 package com.r3.conclave.integrationtests.general.tests
 
+import com.r3.conclave.common.EnclaveMode
 import com.r3.conclave.integrationtests.general.common.tasks.*
 import com.r3.conclave.integrationtests.general.common.threadWithFuture
 import com.r3.conclave.integrationtests.general.common.toByteArray
 import com.r3.conclave.integrationtests.general.common.toInt
 import com.r3.conclave.integrationtests.general.commontest.AbstractEnclaveActionTest
+import com.r3.conclave.integrationtests.general.commontest.TestUtils
 import com.r3.conclave.integrationtests.general.commontest.TestUtils.graalvmOnlyTest
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.Assumptions
+import org.junit.jupiter.api.Assumptions.assumeTrue
 import org.junit.jupiter.api.Test
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.atomic.AtomicInteger
@@ -89,6 +93,12 @@ class ThreadSafeEnclaveTests : AbstractEnclaveActionTest("com.r3.conclave.integr
 
     @Test
     fun `threading inside enclave`() {
+        //  For Gramine, we want this test to run only in SIMULATION mode
+        // CON-1270: Requesting too many threads is failing in Gramine
+        if (TestUtils.RuntimeType.GRAMINE.name == System.getProperty("runtimeType").uppercase()) {
+            assumeTrue(EnclaveMode.SIMULATION.name == System.getProperty("enclaveMode").uppercase())
+        }
+
         val n = 38 // <= thread safe enclave maxThreads (40)
         val result = callEnclave(TooManyThreadsRequestedAction(n))
         assertThat(result).isEqualTo((n * (n + 1)) / 2)
