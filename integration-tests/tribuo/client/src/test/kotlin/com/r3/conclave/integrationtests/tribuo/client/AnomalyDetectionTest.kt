2d3a05f29f4f4bf607d933fbf8b30ba486cbbd0a
Anna Paskova <anna.paskova@r3.com>
2022-12-21 15:50:09 +0000

    Disable Tribuo anomaly test for debug mode in Gramine

--- integration-tests/tribuo/client/src/test/kotlin/com/r3/conclave/integrationtests/tribuo/client/AnomalyDetectionTest.kt
+++ integration-tests/tribuo/client/src/test/kotlin/com/r3/conclave/integrationtests/tribuo/client/AnomalyDetectionTest.kt
@@ -1,5 +1,7 @@
 package com.r3.conclave.integrationtests.tribuo.client
 
+import com.r3.conclave.integrationtests.general.commontest.TestUtils
+import com.r3.conclave.integrationtests.general.commontest.TestUtils.simulationOnlyTest
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.data.Offset
 import org.junit.jupiter.api.*
@@ -33,6 +35,10 @@ class AnomalyDetectionTest : TribuoTest() {
     @Order(1)
     @Test
     fun confusionMatrix() {
+        // Test fails for Gramine in debug mode. CON-1280: Tribuo AnomalyDetectionTest fails for Gramine in debug mode
+        if (TestUtils.runtimeType == TestUtils.RuntimeType.GRAMINE) {
+            simulationOnlyTest()
+        }
         val confusionMatrix = anomalyDetection.confusionMatrix().trim()
         assertThat(confusionMatrix).isEqualTo("""
             EXPECTED  ANOMALOUS
