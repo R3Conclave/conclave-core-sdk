7bbbe3f5623b76def61a0aec0f788f1673a6f495
Marco Bonifazi <85171431+bonifazi-r3@users.noreply.github.com>
2023-01-12 16:36:22 +0000

    CON-1285: Allow back the usage of signing key outside of enclaves
    In previous PRs we have forced the signing keys to be defined inside the enclave directories. This was due to Gramine tests that needed such change.
    In this PR we reverted back to the previous behaviour, so that we can now use signing key files which are outside the enclave paths.

--- plugin-enclave-gradle/src/main/kotlin/com/r3/conclave/plugin/enclave/gradle/gramine/GenerateGramineBundle.kt
+++ plugin-enclave-gradle/src/main/kotlin/com/r3/conclave/plugin/enclave/gradle/gramine/GenerateGramineBundle.kt
@@ -159,12 +159,18 @@ open class GenerateGramineBundle @Inject constructor(
      * This will create a .manifest.sgx and a .sig files into the output dir.
      */
     private fun generateSgxManifestAndSigstruct() {
-        execCommand(
-            "gramine-sgx-sign",
-            "--manifest=${GRAMINE_MANIFEST}",
-            "--key=${signingKey.get().asFile.absolutePath}",
-            "--output=${GRAMINE_SGX_MANIFEST}"
-        )
+        try {
+            val signingKeyPath = linuxExec.prepareFile(signingKey.get().asFile.toPath())
+
+            execCommand(
+                "gramine-sgx-sign",
+                "--manifest=${GRAMINE_MANIFEST}",
+                "--key=${signingKeyPath.absolutePathString()}",
+                "--output=${GRAMINE_SGX_MANIFEST}"
+            )
+        } finally {
+            linuxExec.cleanPreparedFiles()
+        }
     }
 
     /**
